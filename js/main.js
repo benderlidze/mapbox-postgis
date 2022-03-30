9fbdbd867312fef4012424e283d29819bbd14722
Serg <benderlio@gmail.com>
2022-03-30 22:03:08 +0300

    updates

--- js/main.js
+++ js/main.js
@@ -1049,16 +1049,14 @@ function isInt(value) {
 }
 
 
-function autocomplete(inp, data) {
+function autocompleteSimple(inp, data, callback) {
     /*the autocomplete function takes two arguments,
     the text field element and an array of possible autocompleted values:*/
     var currentFocus;
 
-    console.log('data', data);
     const arr = data.data.map(d => {
         return `${d.p_name}, ${d.country}`
     });
-    console.log('arr', arr);
 
     /*execute a function when someone writes in the text field:*/
     inp.addEventListener("input", function (e) {
@@ -1106,10 +1104,138 @@ function autocomplete(inp, data) {
 
                     const poly = data.data[id];
 
-                    inp.value = `${poly.p_name}, ${poly.country}`
+                    inp.value = `${poly.p_name}, ${poly.country} ,${poly.p_id}`
 
                     if (poly) {
+                        if (callback && typeof callback === 'function') {
+                            callback()
+                        }
+                    }
 
+                });
+                if (counter > 20) return; // display forst 20 results
+                a.appendChild(b);
+                counter++;
+            }
+        }
+    });
+    /*execute a function presses a key on the keyboard:*/
+    inp.addEventListener("keydown", function (e) {
+        var x = document.getElementById(this.id + "autocomplete-list");
+        if (x) x = x.getElementsByTagName("div");
+        if (e.keyCode == 40) {
+            /*If the arrow DOWN key is pressed,
+            increase the currentFocus variable:*/
+            currentFocus++;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 38) { //up
+            /*If the arrow UP key is pressed,
+            decrease the currentFocus variable:*/
+            currentFocus--;
+            /*and and make the current item more visible:*/
+            addActive(x);
+        } else if (e.keyCode == 13) {
+            /*If the ENTER key is pressed, prevent the form from being submitted,*/
+            e.preventDefault();
+            if (currentFocus > -1) {
+                /*and simulate a click on the "active" item:*/
+                if (x) x[currentFocus].click();
+            }
+        }
+    });
+    function addActive(x) {
+        /*a function to classify an item as "active":*/
+        if (!x) return false;
+        /*start by removing the "active" class on all items:*/
+        removeActive(x);
+        if (currentFocus >= x.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (x.length - 1);
+        /*add class "autocomplete-active":*/
+        x[currentFocus].classList.add("autocomplete-active");
+
+        console.log('x', x);
+    }
+    function removeActive(x) {
+        /*a function to remove the "active" class from all autocomplete items:*/
+        for (var i = 0; i < x.length; i++) {
+            x[i].classList.remove("autocomplete-active");
+        }
+    }
+    function closeAllLists(elmnt) {
+        /*close all autocomplete lists in the document,
+        except the one passed as an argument:*/
+        var x = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < x.length; i++) {
+            if (elmnt != x[i] && elmnt != inp) {
+                x[i].parentNode.removeChild(x[i]);
+            }
+        }
+    }
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) {
+        closeAllLists(e.target);
+    });
+}
+
+function autocomplete(inp, data) {
+    /*the autocomplete function takes two arguments,
+    the text field element and an array of possible autocompleted values:*/
+    var currentFocus;
+
+    const arr = data.data.map(d => {
+        return `${d.p_name}, ${d.country}`
+    });
+
+    /*execute a function when someone writes in the text field:*/
+    inp.addEventListener("input", function (e) {
+        var a, b, i, val = this.value;
+        /*close any already open lists of autocompleted values*/
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        /*create a DIV element that will contain the items (values):*/
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        /*append the DIV element as a child of the autocomplete container:*/
+        this.parentNode.appendChild(a);
+
+        let counter = 0;
+
+        /*for each item in the array...*/
+        for (i = 0; i < arr.length; i++) {
+
+            /*check if the item starts with the same letters as the text field value:*/
+            if (arr[i] && arr[i].toUpperCase().search(val.toUpperCase()) > -1) {
+
+                /*create a DIV element for each matching element:*/
+                b = document.createElement("DIV");
+                /*make the matching letters bold:*/
+
+                const start = arr[i].toUpperCase().search(val.toUpperCase())
+                const end = start + val.length
+
+                b.innerHTML = arr[i].substr(0, start);
+                b.innerHTML += "<strong>" + arr[i].substr(start, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(end, arr[i].length);
+
+
+                /*insert a input field that will hold the current array item's value:*/
+                b.innerHTML += "<input type='hidden' id=" + i + " value='" + arr[i] + "'>";
+                /*execute a function when someone clicks on the item value (DIV element):*/
+                b.addEventListener("click", function (e) {
+                    /*insert the value for the autocomplete text field:*/
+                    id = this.getElementsByTagName("input")[0].id;
+                    /*close the list of autocompleted values,
+                    (or any other open lists of autocompleted values:*/
+                    closeAllLists();
+
+                    const poly = data.data[id];
+
+                    inp.value = `${poly.p_name}, ${poly.country} ,${poly.p_id}`
+
+                    if (poly) {
                         console.log('poly', poly);
                     }
 
